38865c5d43fd025c9b80c8b20556777f8d9c1460
Alex Yashin <alex-yashin@ya.ru>
2023-05-23 11:12:05 +0400

    дополнительные мета-данные для статьи

--- src/ResourceTypes/ArticleResource.php
+++ src/ResourceTypes/ArticleResource.php
@@ -17,6 +17,9 @@ use Pina\Http\Location;
 use Pina\ResourceManagerInterface;
 use Pina\StaticResource\Style;
 
+use PinaMedia\Media;
+use PinaMedia\MediaGateway;
+
 use function Pina\__;
 
 class ArticleResource implements ResourceTypeInterface
@@ -27,16 +30,30 @@ class ArticleResource implements ResourceTypeInterface
         return __('Статья');
     }
 
+    /**
+     * @param int $id
+     * @return Control
+     * @throws \Exception
+     */
     public function draw(int $id): Control
     {
         $resources = App::container()->get(ResourceManagerInterface::class);
         $resources->append((new Style())->setSrc('static/default/css/editor-content.css'));
         $article = ArticleGateway::instance()
+            ->whereBy('enabled', 'Y')
             ->select('text')
+            ->select('title')
             ->innerJoin(
                 ResourceGateway::instance()->on('id', 'id')
-                    ->onBy('enabled', 'Y')
-                    ->select('title')
+                    ->select('meta_title')
+                    ->select('meta_description')
+                    ->select('meta_keywords')
+            )
+            ->leftJoin(
+                MediaGateway::instance()
+                    ->on('id', 'media_id')
+                    ->selectAs('path', 'media_path')
+                    ->selectAs('storage', 'media_storage')
             )
             ->findOrFail($id);
 
@@ -46,6 +63,18 @@ class ArticleResource implements ResourceTypeInterface
         $view->append((new RawHtml())->setText($article['text']));
 
         Request::setPlace('page_header', $article['title']);
+        Request::setPlace('meta_title', !empty($article['meta_title']) ? $article['meta_title'] : $article['title']);
+        Request::setPlace('meta_description', $article['meta_description']);
+        Request::setPlace('meta_keywords', $article['meta_keywords']);
+
+        Request::setPlace('og_type', 'article');
+        Request::setPlace('og_title', !empty($article['meta_title']) ? $article['meta_title'] : $article['title']);
+        Request::setPlace('og_description', $article['meta_description']);
+        if (!empty($article['media_storage']) && !empty($article['media_path'])) {
+            Request::setPlace('og_image', Media::getUrl($article['media_storage'], $article['media_path']));
+        } else {
+            Request::setPlace('og_image', '');
+        }
 
         return $view;
     }
