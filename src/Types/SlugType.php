4a123c020ea45973403733df7a5e9f3adc1ca043
Alex Yashin <alex-yashin@ya.ru>
2023-07-03 15:38:22 +0400

    добавляем ссылки на страницу с формы редактирования

--- src/Types/SlugType.php
+++ src/Types/SlugType.php
@@ -2,12 +2,29 @@
 
 namespace PinaCMS\Types;
 
+use Pina\App;
+use Pina\Html;
 use Pina\Types\StringType;
+use PinaCMS\SQL\ResourceGateway;
+use PinaCMS\SQL\ResourceUrlGateway;
 
 class SlugType extends StringType//UUIDType
 {
     public function getSize(): int
     {
         return 40;//TODO: сократить до размера UUID 36
+    }
+
+    public function draw($value): string
+    {
+        $url = ResourceUrlGateway::instance()
+            ->innerJoin(
+                ResourceGateway::instance()
+                    ->on('id', 'id')
+                    ->onBy('slug', $value)
+            )
+            ->value('url');
+        return Html::a($value, $url ? App::link($url) : '#', ['target' => '_blank']);
+
     }
 }
